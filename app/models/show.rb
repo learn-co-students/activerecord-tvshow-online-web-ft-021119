e23e168eac8c8004af6fa39fb9b9dea172acfd49
Lizzie <lchan217@gmail.com>
2019-03-28 12:19:44 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,23 @@
 class Show < ActiveRecord::Base
+  def self.highest_rating
+     maximum(:rating)
+  end
+  def self.most_popular_show
+    Show.where("rating > ?", 5).first
+  end
+  def self.lowest_rating
+    minimum(:rating)
+  end
+  def self.least_popular_show
+    Show.where("rating < ?", 5).first
+  end
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+  def self.popular_shows
+    Show.where("rating > ?", 5)
+  end
+  def self.shows_by_alphabetical_order
+    Show.order("name")
+  end
 end
